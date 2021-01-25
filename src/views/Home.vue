2aac54d78fb6b970dfe4b0ac8cb04e93ac6ff1fc
王鹏 <wangpeng_f@aspirecn.com>
2021-01-26 00:07:56 +0800

    接口改为请求服务器本地

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,7 @@ export default {
     }
   },
   mounted() {
-    axios.get('/api/users:3100/api/users').then(res => {
+    axios.get('/api/users').then(res => {
       this.users = res.data
     })
   }
